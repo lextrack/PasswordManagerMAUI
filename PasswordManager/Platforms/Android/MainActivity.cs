0366a9ccbb5508615680a3390e28b778f933ebfa
lextrack <arielmunozg97@gmail.com>
2025-02-02 17:20:14 -0300

    Avoid screenshots system

--- PasswordManager/Platforms/Android/MainActivity.cs
+++ PasswordManager/Platforms/Android/MainActivity.cs
@@ -1,10 +1,34 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using Android.Views;
 
 namespace PasswordManager
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+#if ANDROID
+            // Avoid screenshots
+            Window.SetFlags(WindowManagerFlags.Secure, WindowManagerFlags.Secure);
+#endif
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+
+#if ANDROID
+            // Try to occult the app
+            if (!IsFinishing)
+            {
+                MoveTaskToBack(true);
+            }
+#endif
+        }
     }
 }
